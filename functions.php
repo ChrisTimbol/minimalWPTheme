6839cb5b2389b5b7f0be22e2f12132fb36a09b21
Chris Timbol <christimbol707@gmail.com>
2023-07-31 18:57:40 -0700

    modulized header styling and added to functions

--- functions.php
+++ functions.php
@@ -1,21 +1,48 @@
 <?php
 
 /* enqueues style.css to wordpress */
-function theme_files()
-{
-    wp_enqueue_style( 'mytheme-style', get_theme_file_uri('/css/style.css') ); 
+function theme_files(){
+    wp_enqueue_style( 'main-style', get_theme_file_uri('/css/style.css')); 
+    wp_enqueue_style( 'header-style', get_theme_file_uri('/css/header.css')); 
+   
 }
 add_action('wp_enqueue_scripts', 'theme_files');
  
-function register_my_menus(){
+function register_my_menus(){ // registers menu location in WP 
     register_nav_menus(
         array(
-            'header-menu' =>__( 'Header Menu'),
-            'extra-menu' => __( 'Extra Menu' )
+            'header-menu' =>__( 'Header Menu'), // creates a header-menu var location to refer to in wp
         )
         );
 }
 
-add_action('init', 'register_my_menus')
+add_action('init', 'register_my_menus') // add to wp
 
+//http://minimalistic-site.local/
+//http://minimalistic-site.local/wp-admin/nav-menus.php?menu=16
+//https://chat.openai.com/
+
+
+/*
+    post or page template?
+    figure out links to pages and what the pages will be laid out
+    links on post buttons
+    portfolio layout
+    add option to put blog on a seperate page
+    add about page
+    font
+    color scheme
+
+    // Page layout
+    // Fix navbar layout and buttons
+    // find a nice title font and research font in wordpress
+    // complete single.php  for single post
+    // create a 
+        // page-about.php
+        // page-portfolio.php
+        // create a features tab with a dropdown to files
+        // search bar
+    // fix buttons
+*/
 ?>
+
