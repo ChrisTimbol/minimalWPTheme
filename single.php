af4412d40ce5561c7d53578eb8efdcef4b422446
Chris Timbol <christimbol707@gmail.com>
2023-08-06 16:18:17 -0700

    add single php for post

--- single.php
+++ single.php
@@ -5,15 +5,15 @@
  */
 get_header();
 ?>
-<main class="page-main">
-  <section class="page-container">
+<main class="e-main">
+  <section class="post-container">
     <?php
     if (have_posts()) {
       while (have_posts()) {
-        the_post();
+        the_post(); // What is the_post? and how does it work?
     ?>
         <article class="page-post">
-            THIS IS SINGLE.PHP
+            This is using single.php
           <h2 class="page-title"><?php the_title(); ?></h2>
           <div class="page-details">
             posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?>
