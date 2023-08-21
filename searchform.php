1eb163d99e5f0bed6d04fff12bb9fac419a991fe
Chris Timbol <christimbol707@gmail.com>
2023-08-21 12:44:27 -0700

    esc url for form action

--- searchform.php
+++ searchform.php
@@ -2,6 +2,6 @@
     Custom HTML for get_search_form() called in sidebar-header 
 -->
 
-<form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
     <input type="search" class="search-field" placeholder="Search" value="" name="s" title="Search for:" />
 </form>
