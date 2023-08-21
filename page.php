335ea7f8f1fbf16307d3a81335f88b9a3cf79a99
Chris Timbol <christimbol707@gmail.com>
2023-08-20 18:53:09 -0700

    spacing for page.php

--- page.php
+++ page.php
@@ -1,25 +1,25 @@
 <?php
 get_header();
 ?>
+<?php
+if (have_posts()) :
+    while (have_posts()) : the_post();
+?>
+        <div class="page-container">
+            <header class="page-header">
+                <h2 class="page-title">
+                    <?php the_title(); ?>
+                </h2>
+            </header>
+            <article class="page-content">
+                <?php the_content(); ?>
+            </article>
     <?php
-    if (have_posts()) :
-        while (have_posts()) : the_post();
+    endwhile;
+else :
+    echo '<p>No page found.</p>';
+endif;
     ?>
-<div class="page-container">
-        <header class="page-header">
-            <h2 class="page-title">
-                <?php the_title(); ?>
-            </h2>
-        </header>
-        <div class="page-content">
-            <?php the_content(); ?>
+    </section>
         </div>
-    <?php
-        endwhile;
-    else: 
-        echo '<p>No page found.</p>';
-    endif;
-    ?>
-</section>
-</div>
-<?php get_footer(); ?>
+        <?php get_footer(); ?>
