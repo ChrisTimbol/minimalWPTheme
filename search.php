a814e56302067ac844504d14e397f203d9f3d51f
Chris Timbol <christimbol707@gmail.com>
2023-08-21 12:41:54 -0700

    esc_html added to get functions

--- search.php
+++ search.php
@@ -7,7 +7,7 @@ get_header();
 
 <section class="blog-container">
     <h1 class="search-title">
-        Search Results for: <?php echo get_search_query(); ?>
+        Search Results for: <?php echo esc_html(get_search_query()); ?>
     </h1>
 
 
@@ -25,7 +25,7 @@ get_header();
             </a>
           </h2>
           <p class="date-category">
-            posted in <?php echo get_the_category_list(', '); ?> on
+            posted in <?php echo esc_html(get_the_category_list(', ')); ?> on
             <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
               <?php the_time('F j, Y'); ?>
             </a>
@@ -38,7 +38,7 @@ get_header();
         <div class="post-description">
           <?php the_excerpt(); ?>
         </div>
-        <a class="continueButton" href="<?php the_permalink(); ?>">Continue reading -></a>
+        <a class="continueButton" href="<?php echo esc_url(get_permalink()); ?>">Continue reading -></a>
       </article>
   <?php
     endwhile;
