4c10d957c4f7cf9b258653f83c3f971266a5708c
Chris Timbol <christimbol707@gmail.com>
2023-08-24 08:35:10 -0700

    add hover effect for project w/ title excerpt

--- portfolio.php
+++ portfolio.php
@@ -36,32 +36,34 @@ get_header() ?>
     </ul>
 
     <section class="project-grid-container">
-        <?php
-        $custom_query = new WP_Query(array(
-            'post_type'      => 'project',
-            'posts_per_page' => -1,
-        ));
-        if ($custom_query->have_posts()) :
-            while ($custom_query->have_posts()) : $custom_query->the_post();
-                $categories = get_the_terms(get_the_ID(), 'project_category'); // Get category terms for each project
-        ?>
-                <a href="<?php the_permalink(); ?>" class="thumbnail-container">
-                    <?php if ($categories && !is_wp_error($categories)) : // Adds a span label of the category name of each Project 
-                    ?>
-                        <?php foreach ($categories as $category) : ?>
-                            <span class="category-label"><?php echo esc_html($category->slug); ?></span>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
-                    <?php the_post_thumbnail('medium', ['class' => 'thumbnail-img']); // Project Image 
-                    ?>
-                </a>
-        <?php
-            endwhile;
-            wp_reset_postdata(); // Required after every custom query loop
-        else :
-            esc_html_e('No posts found.', 'minimal-text-domain');
-        endif;
-        ?>
-    </section>
+    <?php
+    $custom_query = new WP_Query(array(
+        'post_type'      => 'project',
+        'posts_per_page' => -1,
+    ));
+    if ($custom_query->have_posts()) :
+        while ($custom_query->have_posts()) : $custom_query->the_post();
+            $categories = get_the_terms(get_the_ID(), 'project_category'); // Get category terms for each project
+    ?>
+            <a href="<?php the_permalink(); ?>" class="thumbnail-container">
+                <?php the_post_thumbnail('medium', ['class' => 'thumbnail-img']); // Project Image ?>
+                
+                <?php if ($categories && !is_wp_error($categories)) : // Adds a span label of the category name of each Project ?>
+                    <?php foreach ($categories as $category) : ?>
+                        <span class="category-label"><?php echo esc_html($category->slug); ?></span>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+                
+                <div class="project-description"><?php the_excerpt(); ?></div>
+            </a>
+    <?php
+        endwhile;
+        wp_reset_postdata(); // Required after every custom query loop
+    else :
+        esc_html_e('No posts found.', 'minimal-text-domain');
+    endif;
+    ?>
+</section>
+>
 </section>
 <?php get_footer(); ?>
