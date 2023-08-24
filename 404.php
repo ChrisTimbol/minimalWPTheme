e3b7411779ab59979a5b36891c563900bcd59700
Chris Timbol <christimbol707@gmail.com>
2023-08-24 07:29:58 -0700

    center 404 page

--- 404.php
+++ 404.php
@@ -4,19 +4,17 @@
  */
 get_header(); ?>
 
-<main class="main-container">
-    <section class="page-container">
+<section class="four-container">
+
         <h1 class="title">Oops! Page Not Found</h1>
         <p>It looks like the page you're trying to reach doesn't exist or has been moved. Please use the navigation menu to find what you're looking for, or try searching the site:</p>
-        
-        <!-- Search Form -->
         
         <span class="search-container navlinks">
                 <?php get_search_form(); ?>
         </span> 
         <!-- Link to Home Page -->
-        <p>Or, you can simply <a href="<?php echo esc_url(home_url('/')); ?>">return to the homepage</a>.</p>
-    </section>
-</main>
+        <p>Or, you can simply <a class="backHome" href="<?php echo esc_url(home_url('/')); ?>">return to the homepage</a>.</p>
+
+</section>
 
 <?php get_footer(); ?>
