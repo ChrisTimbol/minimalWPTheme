01dbbd5b4275d6a23131174977249f5451af7f07
Chris Timbol <christimbol707@gmail.com>
2023-08-27 15:33:07 -0700

    blog-layout-1 same lay out as index.php

--- blog-layout-1.php
+++ blog-layout-1.php
@@ -1,11 +1,11 @@
 <?php
 /*
-* Template Name: Blog layout no sidebar
+* Template Name: Blog layout no sidebar( same as index.php)
 *
 */
 get_header();
 ?>
-<main class="blog-container-1">
+<main class="blog-container">
 
   <?php $query = new WP_Query(array(
     'post_type'      => 'post',
@@ -15,36 +15,37 @@ get_header();
     while ($query->have_posts()) : $query->the_post();
   ?>
       <article class="post-container">
-
         <header class="post-header">
           <h2>
-            <a class="title" href="<?php echo esc_url(get_permalink()); ?>">
+            <a class="post-title" href="<?php echo esc_url(get_permalink()); ?>">
               <?php the_title(); ?>
             </a>
           </h2>
-          <p class="date-category">
-            posted in <?php echo get_the_category_list(', '); ?> on
-            <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
-              <?php the_time('F j, Y'); ?>
-            </a>
-            by <?php the_author_posts_link(); ?>
-            <a href="<?php echo esc_url(get_comments_link()); ?>">
-              <?php comments_number('0 comments', '1 comment', '% comments'); ?>
-            </a>
-          </p>
+          <div class="post-meta">
+            <span class="post-category">posted in <?php echo get_the_category_list(', '); ?> on</span>
+            <time class="post-date" datetime="<?php echo get_the_date('c'); ?>">
+              on <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
+                <?php the_time('F j, Y'); ?>
+              </a>
+              <span class="post-author">by <?php the_author_posts_link(); ?></span>
+              <span class="post-comments">
+                <a href="<?php echo esc_url(get_comments_link()); ?>">
+                  <?php comments_number('0 comments', '1 comment', '% comments'); ?>
+                </a>
+              </span>
+          </div>
+
         </header>
         <div class="post-description">
           <?php the_excerpt(); ?>
         </div>
         <a class="continueButton" href="<?php the_permalink(); ?>">Continue reading -></a>
       </article>
+
   <?php
     endwhile;
     wp_reset_postdata();
-  else :
-  // No posts found message
   endif;
-
   ?>
   <div class="pagination-wrapper">
     <?php
@@ -57,5 +58,6 @@ get_header();
     ));
     ?>
   </div>
-  </section>
-  <?php get_footer(); ?>
+
+</main>
+<?php get_footer(); ?>
