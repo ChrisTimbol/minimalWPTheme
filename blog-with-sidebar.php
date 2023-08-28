1b2cd8f85bdc46f731d5157fdbe077b71c108203
Chris Timbol <christimbol707@gmail.com>
2023-08-27 18:53:55 -0700

    remove unnecessary sidebar

--- blog-with-sidebar.php
+++ blog-with-sidebar.php
@@ -5,65 +5,65 @@
 get_header();
 ?>
 <main class="blog-container-with-sidebar">
-    <section class="blog-container-2">
-        <?php $query = new WP_Query(array(
-            'post_type'      => 'post',
-            'posts_per_page' => -1,
-        ));
-        if ($query->have_posts()) :
-            while ($query->have_posts()) : $query->the_post();
-        ?>
-      <article class="post-container">
-        <header class="post-header">
-          <h2 class="post-title" >
-            <a href="<?php echo esc_url(get_permalink()); ?>">
-              <?php the_title(); ?>
-            </a>
-          </h2>
-          <div class="post-meta">
-            <span class="post-category">posted in <?php echo get_the_category_list(', '); ?> on</span>
-            <time class="post-date" datetime="<?php echo get_the_date('c'); ?>">
-              on <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
-                <?php the_time('F j, Y'); ?>
-              </a>
-              <span class="post-author">by <?php the_author_posts_link(); ?></span>
-              <span class="post-comments">
-                <a href="<?php echo esc_url(get_comments_link()); ?>">
-                  <?php comments_number('0 comments', '1 comment', '% comments'); ?>
-                </a>
-              </span>
-          </div>
-
-        </header>
-        <div class="post-description">
-          <?php the_excerpt(); ?>
-        </div>
-        <a class="continueButton" href="<?php the_permalink(); ?>">Continue reading -></a>
-      </article>
-
-  <?php
-    endwhile;
-    wp_reset_postdata();
-  endif;
-  ?>
-  <section class="pagination-wrapper">
-    <?php
-    the_posts_pagination(array(
-      'mid_size'           => 2,
-      'prev_text'          => __('&laquo; Previous', 'text-domain'),
-      'next_text'          => __('Next &raquo;', 'text-domain'),
-      'screen_reader_text' => __('Posts Navigation', 'text-domain'),
-      'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'text-domain') . ' </span>',
+  <section class="blog-container-2">
+    <?php $query = new WP_Query(array(
+      'post_type'      => 'post',
+      'posts_per_page' => -1,
     ));
+    if ($query->have_posts()) :
+      while ($query->have_posts()) : $query->the_post();
     ?>
+        <article class="post-container">
+          <header class="post-header">
+            <h2 class="post-title">
+              <a href="<?php echo esc_url(get_permalink()); ?>">
+                <?php the_title(); ?>
+              </a>
+            </h2>
+            <div class="post-meta">
+              <span class="post-category">posted in <?php echo get_the_category_list(', '); ?> on</span>
+              <time class="post-date" datetime="<?php echo get_the_date('c'); ?>">
+                on <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
+                  <?php the_time('F j, Y'); ?>
+                </a>
+                <span class="post-author">by <?php the_author_posts_link(); ?></span>
+                <span class="post-comments">
+                  <a href="<?php echo esc_url(get_comments_link()); ?>">
+                    <?php comments_number('0 comments', '1 comment', '% comments'); ?>
+                  </a>
+                </span>
+            </div>
+
+          </header>
+          <div class="post-description">
+            <?php the_excerpt(); ?>
+          </div>
+          <a class="continueButton" href="<?php the_permalink(); ?>">Continue reading -></a>
+        </article>
+
+    <?php
+      endwhile;
+      wp_reset_postdata();
+    endif;
+    ?>
+    <section class="pagination-wrapper">
+      <?php
+      the_posts_pagination(array(
+        'mid_size'           => 2,
+        'prev_text'          => __('&laquo; Previous', 'text-domain'),
+        'next_text'          => __('Next &raquo;', 'text-domain'),
+        'screen_reader_text' => __('Posts Navigation', 'text-domain'),
+        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'text-domain') . ' </span>',
+      ));
+      ?>
+    </section>
   </section>
-</section>
   <aside class="blog-container-sidebar">
-        <?php if (is_active_sidebar('blog-sidebar')) : ?>
-            <?php dynamic_sidebar('blog-sidebar'); ?>
-        <?php else : ?>
-            <p>Please add widgets to the sidebar.</p>
-        <?php endif; ?>
-    </aside>
+    <?php if (is_active_sidebar('blog-sidebar')) : ?> <!-- Name of sidebar in functions.php -->
+      <?php dynamic_sidebar('blog-sidebar'); ?> <!-- Generates it based on parameters in functions.php  -->
+    <?php else : ?>
+      <p>Please add widgets to the sidebar rom Appearance > Widgets</p>
+    <?php endif; ?>
+  </aside>
 </main>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -4,9 +4,10 @@
  */
 
 
-add_theme_support( 'title-tag' );
+add_theme_support('title-tag');
 add_theme_support('post-thumbnails'); /* Allow post-thumbnails , main usage is for portfolio images */
-function theme_files() { /* this function is use to enqueue Styling, fonts, and .js scripts into wordpress */
+function theme_files()
+{ /* this function is use to enqueue Styling, fonts, and .js scripts into wordpress */
     // css files 
     wp_enqueue_style('main-style', get_theme_file_uri('/css/index.css'));
     wp_enqueue_style('header-style', get_theme_file_uri('/css/header.css'));
@@ -22,16 +23,16 @@ function theme_files() { /* this function is use to enqueue Styling, fonts, and 
     // Enqueue Google Fonts
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=UnifrakturMaguntia&display=swap');
     // Only include the script on the portfolio template page
-    if (is_page_template('portfolio.php')) { 
+    if (is_page_template('portfolio.php')) {
         wp_enqueue_script('category-filter', get_template_directory_uri() . '/js/portfolio-category-filter.js', array(), null, true);
     }
 
     /* Enqueue hamburger js click */
     wp_enqueue_script('hamburger-script', get_template_directory_uri() . '/js/hamburger.js', array('jquery'), '1.0.0', true);
-
 }
-add_action('wp_enqueue_scripts', 'theme_files'); 
-function register_project_post_type() { // adds project post type to admin bar
+add_action('wp_enqueue_scripts', 'theme_files');
+function register_project_post_type()
+{ // adds project post type to admin bar
     $labels = array(
         'name'                  => _x('Projects', 'Post Type General Name', 'Minimalistic'),
         'singular_name'         => _x('Projects', 'Post Type Singular Name', 'Minimalistic'),
@@ -46,12 +47,13 @@ function register_project_post_type() { // adds project post type to admin bar
         'show_in_menu'          => true,
         'menu_position'         => 4,
         'supports' => array('title', 'editor', 'thumbnail'),
-        
+
     );
     register_post_type('project', $args);
 }
 add_action('init', 'register_project_post_type');
-function custom_project_category_taxonomy() { /* Custom category taxonomy for projects */
+function custom_project_category_taxonomy()
+{ /* Custom category taxonomy for projects */
     $labels = array(
         'name' => 'Project Categories',
         'singular_name' => 'Project Category',
@@ -77,56 +79,63 @@ function custom_project_category_taxonomy() { /* Custom category taxonomy for pr
     register_taxonomy('project_category', 'project', $args); /* (taxonomy name, associatedWithPostType, $args) */
 }
 add_action('init', 'custom_project_category_taxonomy');
-function my_custom_sidebars_init() { /* Use to create sidebar for header, needs work */
+function my_custom_sidebars_init()
+{ /* Use to create sidebar for header, needs work */
     register_sidebar(
         array(
-            'name'          => 'Blog sidebar/widget area',
-            'id'            => 'blog-sidebar',
+            'name'          => 'Blog sidebar area',
+            'id'            => 'blog-sidebar', /* referred in blog-with-sidebar layou */
             'description'   => 'area next to navbar links',
             'before_widget' => '<section class="widget">',
             'after_widget'  => '</section>',
-            'before_title'  => '<h3 class="widget-title">', 
+            'before_title'  => '<h3 class="widget-title">',
             'after_title'   => '</h3>',
         )
     );
 }
-add_action( 'widgets_init', 'my_custom_sidebars_init' );
-function register_my_menus() { /* Register a menu locationfor user to edit */
+add_action('widgets_init', 'my_custom_sidebars_init');
+function register_my_menus()
+{ /* Register a menu location for user to select in admin menu */
     register_nav_menus(
         array(
-            'primary' => __('Header Menu', 'Minimalistic'),  
+            'primary' => __('Header Menu', 'Minimalistic'),
         )
     );
 }
-add_action('init', 'register_my_menus'); 
-function wpdocs_custom_excerpt_length( $length ) { /* Excerpt length */
-	return 20;
+add_action('init', 'register_my_menus');
+function wpdocs_custom_excerpt_length($length)
+{ /* Limits excerpt length for list of post */
+    return 20;
 }
-add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+add_filter('excerpt_length', 'wpdocs_custom_excerpt_length', 999);
 
 
-function my_footer_menu() { /* Menu for footer layout */
-    register_nav_menu('footer-menu', __( 'Footer Menu', 'Minimalistic'));
+function my_footer_menu()
+{ /* Menu for footer layout */
+    register_nav_menu('footer-menu', __('Footer Menu', 'Minimalistic'));
 }
 add_action('init', 'my_footer_menu');
 
 
-function custom_excerpt_length( $length ) { 
-    if ( get_post_type() == 'project' ) {
-        return 9; // Change 20 to the number of words you want for the 'project' post type
+function custom_excerpt_length($length)
+{
+    if (get_post_type() == 'project') { /* Limits project post excerpt length */
+        return 9;
     }
-    return $length; // Return default length for other post types
+    return $length;
 }
-add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+add_filter('excerpt_length', 'custom_excerpt_length', 999);
 
-function custom_excerpt_ellipsis( $more ) {
+function custom_excerpt_ellipsis($more) /* Removes the [...] from end of excerpt */
+{
     return '';
 }
-add_filter( 'excerpt_more', 'custom_excerpt_ellipsis' );
+add_filter('excerpt_more', 'custom_excerpt_ellipsis');
 
 
-function my_theme_setup() {
+function my_theme_setup() /*  ? */
+{
     // Add default posts and comments RSS feed links to head.
-    add_theme_support( 'automatic-feed-links' );
+    add_theme_support('automatic-feed-links');
 }
-add_action( 'after_setup_theme', 'my_theme_setup' );
+add_action('after_setup_theme', 'my_theme_setup');
