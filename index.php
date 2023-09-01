9b8a28b8b1454ecb47f8d7027de3b8a9179e6bfc
Chris Timbol <christimbol707@gmail.com>
2023-09-01 11:32:49 -0700

    add space fix postmeta

--- index.php
+++ index.php
@@ -19,9 +19,9 @@ get_header();
             </a>
           </h2>
           <div class="post-meta">
-            <span class="post-category">posted in <?php echo get_the_category_list(', '); ?> on</span>
+            <span class="post-category">posted in <?php echo get_the_category_list(', '); ?> </span>
             <time class="post-date" datetime="<?php echo get_the_date('c'); ?>">
-              on <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
+            &nbspon <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
                 <?php the_time('F j, Y'); ?>
               </a>
               <span class="post-author">by <?php the_author_posts_link(); ?></span>
