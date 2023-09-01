5c21ef52df358c375199542356545667bc72df8c
Chris Timbol <christimbol707@gmail.com>
2023-08-31 21:57:31 -0700

    reset wp_link_pages to default with no parameters

--- single.php
+++ single.php
@@ -10,32 +10,28 @@ get_header();
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
             <article class="post-container" <?php post_class(); ?>>
-            <div class="single-post-container">
-                <h2 class="post-title">
-                    <a href="<?php echo esc_url(get_permalink()); ?>">
-                        <?php the_title(); ?>
-                    </a>
-                </h2>
-                <div class="post-meta">
-                    <span class="post-category">posted in <?php echo get_the_category_list(', '); ?> on</span>
-                    <time class="post-date" datetime="<?php echo get_the_date('c'); ?>">
-                        on <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
-                            <?php the_time('F j, Y'); ?>
+                <div class="single-post-container">
+                    <h2 class="post-title">
+                        <a href="<?php echo esc_url(get_permalink()); ?>">
+                            <?php the_title(); ?>
                         </a>
-                    </time>
-                    <span class="post-author">by <?php the_author_posts_link(); ?></span>
+                    </h2>
+                    <div class="post-meta">
+                        <span class="post-category">posted in <?php echo get_the_category_list(', '); ?> on</span>
+                        <time class="post-date" datetime="<?php echo get_the_date('c'); ?>">
+                            on <a href="<?php echo esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d'))); ?>">
+                                <?php the_time('F j, Y'); ?>
+                            </a>
+                        </time>
+                        <span class="post-author">by <?php the_author_posts_link(); ?></span>
 
-                </div>
-
-                <?php the_content(); ?>
-
-                <?php
-                // Display page links for paginated posts
-                wp_link_pages(array(
-                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'minimalistic'),
-                    'after'  => '</div>',
-                ));
-                ?>
+                    </div>
+                    <div class="post-content">
+                        <?php the_content(); ?>
+                    </div>
+                    <div>
+                        <?php wp_link_pages(); ?><!-- enables paginated posts quicktag -->
+                    </div>
                 </div>
                 <div class="post-comments">
                     <?php
@@ -45,6 +41,7 @@ get_header();
                     }
                     ?>
                 </div>
+
             </article>
 
 
